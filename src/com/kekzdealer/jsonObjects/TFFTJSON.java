8239bedf03e5e3529f8ad93a5f00780049060f0b
lucy <sleepylucy12@gmail.com>
2020-05-13 22:20:33 +0200

    removed kekz's stuff for the time being cuz it was only half finished and she wanted to rewrite it anyway

--- src/com/kekzdealer/jsonObjects/TFFTJSON.java
+++
@@ -1,8 +0,0 @@
-package com.kekzdealer.jsonObjects;
-
-import java.util.List;
-
-public class TFFTJSON {
-	
-	public List<Item> fluids;
-}
--- src/com/lucy/ocserver/Main.java
+++ src/com/lucy/ocserver/Main.java
@@ -1,10 +1,5 @@
 package com.lucy.ocserver;
 
-import com.kekzdealer.CraftingSheduler;
-import com.kekzdealer.data.FluidStorage;
-import com.kekzdealer.data.ItemStorage;
-import com.kekzdealer.data.RecipeDatabase;
-import com.kekzdealer.util.Logger;
 import com.lucy.ocserver.httpHandlers.*;
 import com.sun.net.httpserver.HttpServer;
 
@@ -15,35 +10,25 @@ public class Main {
 
     public static void main(String[] args) {
 	    int port = 9876;
-        HttpServer server = null;
+        HttpServer server;
         try {
             server = HttpServer.create(new InetSocketAddress(port), 0);
+
+            if (server == null) {
+                throw new IllegalStateException();
+            }
         } catch (IOException e) {
             e.printStackTrace();
+            return;
         }
-        Logger.INFO.print("Server attached to port: " + port);
+        System.out.println("Server attached to port: " + port);
 
         server.createContext("/", new RootHandler());
         server.createContext("/meStatus", new MeStatusHandler());
         server.createContext("/pssStatus", new PSSstatusHandler());
-        
-        // Chemistry Control
-        Logger.INFO.print("Starting Chemistry Control Server...");
-        final RecipeDatabase recipeDatabase = new RecipeDatabase();    
-        final FluidStorage fluidStorage = new FluidStorage(); 
-        final ItemStorage itemStorage = new ItemStorage();
-        
-        final Thread craftingShedulerThread = new Thread(new CraftingSheduler(fluidStorage, itemStorage));
-        craftingShedulerThread.setName("Chemistry Crafting Sheduler");
-        craftingShedulerThread.start();
-        
-        server.createContext("/chemistry/commit_recipe", new CommitRecipeHandler(recipeDatabase));
-        server.createContext("/chemistry/shedule_craft", null);// TODO create new handler here
-        server.createContext("/chemistry/tfft_data_feed", new TFFTDataFeedHandler(fluidStorage));
-        
-        
+
         server.setExecutor(null);
         server.start();
-        Logger.INFO.print("Ready.");
+        System.out.println("Ready.");
     }
 }
