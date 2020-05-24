d2727981ca016d3686aebf6a54589873a5fa7094
lucy <sleepylucy12@gmail.com>
2020-05-24 23:44:31 +0200

    added more power gen locations

--- src/com/lucy/ocserver/httpHandlers/PowerGenStatusHandler.kt
+++ src/com/lucy/ocserver/httpHandlers/PowerGenStatusHandler.kt
@@ -41,12 +41,18 @@ class PowerGenStatusHandler: HttpHandler {
 
 enum class PowerGenLocation {
     Dam11, Dam12, Dam13, Dam14, Dam15, Dam16, Dam17,
-    Dam21, Dam22, Dam23, Dam24, Dam25, Dam26, Dam27;
+    Dam21, Dam22, Dam23, Dam24, Dam25, Dam26, Dam27,
+
+    ChemN1, ChemN2, ChemE1, ChemE2, ChemS1, ChemS2, ChemW1, ChemW2,
+
+    MountainWind;
 
     fun type(): PowerGenType {
         return when(this) {
             Dam11, Dam12, Dam13, Dam14, Dam15, Dam16, Dam17 -> PowerGenType.Hydro
             Dam21, Dam22, Dam23, Dam24, Dam25, Dam26, Dam27 -> PowerGenType.Hydro
+            ChemN1, ChemN2, ChemE1, ChemE2, ChemS1, ChemS2, ChemW1, ChemW2 -> PowerGenType.Solar
+            MountainWind -> PowerGenType.Wind
         }
     }
 }
